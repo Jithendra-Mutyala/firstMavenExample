86e9f911890275c54f9c5683d4c9ad23c35bb4f5
Jithendra-Mutyala <jithendra.mutyala@lntinfotech.com>
2022-03-14 16:59:27 +0530

    my first commit

---
+++ firstMavenExample/src/main/java/firstMavenExample/Customer.java
@@ -0,0 +1,9 @@
+package firstMavenExample;
+
+public class Customer {
+	private  int cusNo;
+	private String cusName;
+	private double billAmount;
+	
+
+}
---
+++ firstMavenExample/src/main/java/firstMavenExample/TestCal.java
@@ -0,0 +1,16 @@
+package firstMavenExample;
+
+public class TestCal {
+
+	public static void main(String[] args) {
+		
+		Calculator c= new Calculator();
+		int r=c.add(100, 200);
+		System.out.println(r);
+
+
+		// TODO Auto-generated method stub
+
+	}
+
+}
