21fdc5895a0be4bae3bb85d4ae327b3661c56b66
Jithendra-Mutyala <jithendra.mutyala@lntinfotech.com>
2022-03-15 09:45:06 +0530

    Third Commit

--- firstMavenExample/src/main/java/firstMavenExample/TestCal.java
+++ firstMavenExample/src/main/java/firstMavenExample/TestCal.java
@@ -7,6 +7,8 @@ public class TestCal {
 		Calculator c= new Calculator();
 		int r=c.add(100, 200);
 		System.out.println(r);
+		System.out.println("I changed the main method");
+		
 
 
 		// TODO Auto-generated method stub
